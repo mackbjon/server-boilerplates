ded66b5746bcf39230cc40c23be91b693741ec89
mackbjon <48659911+mackbjon@users.noreply.github.com>
2020-09-04 18:27:16 -0400

    Update README

--- express-postgres/src/index.js
+++ express-postgres/src/index.js
@@ -5,7 +5,7 @@ import { PORT } from './utils/config'
 const server = http.createServer(app)
 
 server.listen(PORT, () => {
-  console.log(`server listening on port: ${PORT}`)
+  console.log(`Server listening on port: ${PORT}`)
   if (PORT === 4000) {
     console.log('Server is ready at http://localhost:4000')
   }
